ffd77d2771c4299d160f841e73101ae3a5152a5f
Leonardo J Morales <leom6313@gmail.com>
2025-02-09 16:33:31 -0300

    feat: config react y tailwind

---
+++ src/ReactGPT.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ReactGPT = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        React Gpt
+      </h1>
+    </div>
+  )
+}
+
+export default ReactGPT
